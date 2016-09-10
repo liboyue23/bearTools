4a285714a351bc05aab4bbe1dabd0391483bc759
boyue.lby <boyue.lby@alibaba-inc.com>
2016-09-10 17:03:37 +0800

    first commit

---
+++ bear-qatest/src/test/java/com/bear/guava/TestGuava.java
@@ -0,0 +1,15 @@
+package com.bear.guava;
+
+
+import org.junit.Test;
+
+/**
+ * @author boyue.lby
+ * @date 16/9/10 下午4:26
+ */
+public class TestGuava {
+    @Test
+    public void testOptional(){
+
+    }
+}
